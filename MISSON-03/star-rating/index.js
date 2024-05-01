44800986a3840fff59e6ad719b3f89f31029cd2c
JIHU96 <korjihu@gmail.com>
2024-05-02 00:59:21 +0900

    feat: 미션-03 구현

--- MISSON-03/star-rating/index.js
+++ MISSON-03/star-rating/index.js
@@ -1,1 +1,74 @@
-// do something!
+/**
+ *
+ * @param {Element} $container
+ */
+
+// 한번만 실행하기 위해 index.js 상단에서 css 파일 추가
+const linkElement = document.createElement("link");
+linkElement.rel = "stylesheet";
+linkElement.href = "./star-rating/theme.css";
+document.head.appendChild(linkElement);
+
+// 커스텀 이벤트 또한 중복이기 때문에 한번만 생성
+const customEvent = new CustomEvent("rating-change", { bubbles: true, detail: "0" });
+
+const StarRating = ($container) => {
+  const container = $container;
+
+  /* attribute값만큼 Star 생성 */
+  const starCount = Number($container.getAttribute("data-max-rating"));
+  const starBox = `<i class='bx bxs-star'></i>`;
+  let stars = "";
+  for (let count = 0; count < starCount; count++) {
+    stars += starBox;
+  }
+  const appendStars = `<div class="star-rating-container">${stars}</div>`;
+  container.innerHTML = appendStars;
+
+  /* 각 Star에 hover와 click 이벤트 추가 */
+  const starContainer = container.querySelector(".star-rating-container");
+  // 호버시 호버 클래스 추가
+  const mouseOverStarHandler = (e) => {
+    if (e.target.tagName === "I") {
+      const hoveredIndex = Array.from(starContainer.children).indexOf(e.target);
+      for (let i = 0; i <= hoveredIndex; i++) {
+        const starClassList = starContainer.children[i].classList;
+        if (!starClassList.contains("selected")) starClassList.add("hovered");
+      }
+    }
+  };
+  // 호버 해제시 호버 클래스 제거
+  const mouseOutStarHandler = (e) => {
+    if (e.target.tagName === "I") {
+      const hoveredIndex = Array.from(starContainer.children).indexOf(e.target);
+      for (let i = 0; i <= hoveredIndex; i++) {
+        starContainer.children[i].classList.remove("hovered");
+      }
+    }
+  };
+  // 클릭 이벤트: 클릭된 별까지는 selected 클래스 추가 + hovered 클래스 제거
+  //           기존 높은 rating에서 낮은 rating 클릭 시 그 차이만큼 selected 클래스 제거
+  const clickStarHandler = (e) => {
+    if (e.target.tagName === "I") {
+      const clickedtIndex = Array.from(starContainer.children).indexOf(e.target);
+
+      for (let i = 0; i < starCount; i++) {
+        const classList = starContainer.children[i].classList;
+        if (i <= clickedtIndex) {
+          classList.add("selected");
+          classList.remove("hovered");
+        } else classList.remove("selected");
+      }
+      const customEvent = new CustomEvent("rating-change", {
+        bubbles: true,
+        detail: clickedtIndex + 1,
+      });
+      $container.dispatchEvent(customEvent);
+    }
+  };
+  starContainer.addEventListener("mouseover", mouseOverStarHandler);
+  starContainer.addEventListener("mouseout", mouseOutStarHandler);
+  starContainer.addEventListener("click", clickStarHandler);
+};
+
+export default StarRating;
